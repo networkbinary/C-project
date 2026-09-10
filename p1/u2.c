852363bc1f66adda2e0ecc2981d6bb578f1a4677
networkbinary <networkbinary48@gmail.com>
2026-09-10 22:17:04 +0300

    Add files via upload

---
+++ p1/u2.c
@@ -0,0 +1,18 @@
+/*
+Author:NetworkBinary
+Reg No:BCS-05-0541
+*/
+
+
+//programmes to display Units offered this semester
+#include <stdio.h>
+int main() {
+    printf("Units Offered this Semester:\n");
+    printf("1. Introduction To Programming\n");
+    printf("2. Data Structures\n");
+    printf("3. Computer Networks\n");
+    printf("4. Data Systems\n");
+    printf("5. Software Engineering");
+
+    return 0;
+}
