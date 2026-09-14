1b161387e05dec9bc306f915a50e883db75fca02
networkbinary <networkbinary48@gmail.com>
2026-09-14 03:11:26 -0400

    Refactor comments and formatting in u2.c

--- p1/u2.c
+++ p1/u2.c
@@ -3,7 +3,7 @@ Author:NetworkBinary
 Reg No:BCS-05-0541
 */
 
-//program to display Units offered this semester
+//Program To Display Units Offered This Semester
 #include <stdio.h>
 int main() {
     printf("Units Offered this Semester:\n");
